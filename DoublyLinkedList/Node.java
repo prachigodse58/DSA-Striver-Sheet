3e24c4903b802d173cfe16b149e6d031f9ea5459
prachigodse58 <113751030+prachigodse58@users.noreply.github.com>
2024-09-28 23:58:24 +0530

    Node.java

--- DoublyLinkedList/Node.java
+++ DoublyLinkedList/Node.java
@@ -63,7 +63,6 @@ public class Node {
 		return head;
 	}
 
-
 	public static Node deleteKthElem(Node head, int k) {
 		Node temp = head ;
 
@@ -81,9 +80,7 @@ public class Node {
 		}
 		Node prev = temp.back;
 		Node front= temp.next;
-		if(prev== null ) {
-			head = deleteNode(head);
-		}
+		if(prev== null ) {}
 		else if(front==null) {
 			head= deleteTail(head);
 		}
@@ -93,14 +90,57 @@ public class Node {
 		temp.back = null;
 		return head;
 	}
+
+	public static void deleteNode1(Node temp) {
+
+		Node prev = temp.back;
+		Node front = temp.next;
+		if(front == null) {
+			prev.next = null;
+			temp.back= null;
+		}
+		prev.next = front;
+		front.back= prev;
+		temp.next=temp.back=null;
+	}
+
+	public static Node insertHead(Node head,int val) {
+
+		Node newNode = new Node(val,head,null);
+		head.back=newNode;
+		newNode.next= head;
+		newNode.back= null;
+
+		return newNode;
+	}
+
+	public static Node insertNode(Node head , int val) {
+		
+		Node tail = head;
+		Node newNode = new Node(val);
+
+		while(tail.next != null) {
+			tail=tail.next;
+		}
+		
+		Node prev = tail.back;
+		
+		newNode.next= tail;
+		newNode.back = prev;
+        prev.next=newNode;
+		tail.back= newNode;
+		return head;
+	}
 	public static void main(String[] args) {
 		int[] arr= {2,5,6,7,8};
 		Node head = ArrToDLL(arr);
 		//		head =deleteNode(head);
 		//		head = deleteTail(head);
 		int k = 2;
-		head = deleteKthElem(head, k);
+		//		head = deleteKthElem(head, k);
+		//		deleteNode1(head.next.next);
+		//		head = insertHead(head, 1);
+		head = insertNode(head , 10);
 		print(head);
-
 	}
 }
